0b3f6aee75aab2573584f109c9612a5f39c0c784
BENDALI Manal <116835454+manalbendali@users.noreply.github.com>
2025-02-20 06:05:01 +0100

    Update Merci.java

--- Merci.java
+++ Merci.java
@@ -3,3 +3,17 @@ public class Merci {
         System.out.println(" Merci !");
     }
 }
+
+<form enctype="multipart/form-data">
+  <label for="csvFile">Upload CSV File:</label>
+  <input type="file" id="csvFile" 
+         cam-variable-name="csvFile"
+         cam-variable-type="File"
+         required />
+</form>
+
+
+  <form enctype="multipart/form-data">
+  <label for="csvFile">Upload CSV File:</label>
+  <input type="file" id="csvFile" name="csvFile" required />
+</form>
